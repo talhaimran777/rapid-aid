46401f4c3b1dd48684b11c96fa41db03bf305f47
Talha Imran <talhaimran284@gmail.com>
2021-06-20 18:25:33 +0500

    Worked on the Profile Image and added logout functionality

--- front-end/src/components/profileImage/profile.image.js
+++ front-end/src/components/profileImage/profile.image.js
@@ -1,7 +1,57 @@
-import { useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import md5 from 'md5';
 
+// MATERIAL UI
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import PersonIcon from '@material-ui/icons/Person';
+
+// FOR CUSTOM STYLES
+import { withStyles } from '@material-ui/core/styles';
+
+// UTILS
+import setAuthToken from '../../utils/setAuthToken';
+
+// AUTH ACTIONS
+import { setCurrentUser } from '../../actions/authActions';
+
 const ProfileImage = () => {
+  const dispatch = useDispatch();
+
+  const StyledMenuItem = withStyles({
+    root: {
+      fontWeight: 'bold',
+      color: '#7C3AED',
+    },
+  })(MenuItem);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logout = () => {
+    // Logout user
+    // Remove token from local storage
+    localStorage.removeItem('jwtToken');
+
+    // Remove auth header for future requests
+    setAuthToken(false);
+
+    // Set current user to empty object {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
+
+    // Redirect to login
+    window.location.href = './login';
+  };
+
   const { user } = useSelector((state) => state.auth);
 
   // GETTING EMAIL FROM THE USER OBJECT
@@ -14,13 +64,41 @@ const ProfileImage = () => {
   const url = `https://www.gravatar.com/avatar/${hashedEmail}?s=200`;
 
   return (
-    <img
-      height='35px'
-      width='35px'
-      className=' rounded-full cursor-pointer'
-      src={url}
-      alt=''
-    />
+    <div>
+      <img
+        aria-controls='simple-menu'
+        aria-haspopup='true'
+        onClick={handleClick}
+        height='35px'
+        width='35px'
+        className=' rounded-full cursor-pointer ml-5'
+        src={url}
+        alt=''
+      />
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem
+          className='flex justify-between items-end'
+          onClick={handleClose}
+        >
+          <PersonIcon style={{ fontSize: 24 }} />
+          <h1 className='ml-1'>Profile</h1>
+        </StyledMenuItem>
+
+        <StyledMenuItem
+          className='flex justify-between items-end'
+          onClick={logout}
+        >
+          <ExitToAppIcon style={{ fontSize: 24 }} />
+          <h1 className='ml-1'>Logout</h1>
+        </StyledMenuItem>
+      </Menu>
+    </div>
   );
 };
 
